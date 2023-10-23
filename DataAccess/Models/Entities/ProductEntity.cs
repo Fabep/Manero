3fe54baa4aff2a075f1c459af6d2530849bc621a
Fabep <fabian-bechpersson@hotmail.com>
2023-10-23 10:02:06 +0200

    Base models for Products created.

---
+++ DataAccess/Models/Entities/ProductEntity.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Models.Entities;
+
+public class ProductEntity
+{
+    [Key]
+    public Guid ProductId = Guid.NewGuid();
+    [Required]
+    public string ProductName { get; set; } = null!;
+    [Required]
+    public string ProductDescription { get; set; } = null!;
+    [Required]
+    [Range(0, int.MaxValue)]
+    public decimal ProductPrice { get; set; }
+    public int? Rating { get; set; }
+    public int? Quantity { get; set; }
+
+}
---
+++ DataAccess/Models/Schemas/ProductSchema.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess.Models.Schemas;
+
+public class ProductSchema
+{
+    public string ProductName { get; set; } = null!;
+    [Required]
+    public string ProductDescription { get; set; } = null!;
+    [Required]
+    [Range(0, int.MaxValue)]
+    public decimal ProductPrice { get; set; }
+    public int? Rating { get; set; }
+    public int? Quantity { get; set; }
+}
