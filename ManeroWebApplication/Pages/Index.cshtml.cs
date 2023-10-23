577690f98621ab89b018f2d8bd7b603bfbb97892
patpatpanda <nils-emil1337@hotmail.se>
2023-10-23 16:58:20 +0200

    visar 4 featured-products på sidan

--- ManeroWebApplication/Pages/Index.cshtml.cs
+++ ManeroWebApplication/Pages/Index.cshtml.cs
@@ -1,33 +1,41 @@
-﻿using DataAccess.Models;
+﻿using DataAccess.Contexts;
+using DataAccess.Models;
+using DataAccess.Models.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
 
 namespace ManeroWebApplication.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+		private readonly ILogger<IndexModel> _logger;
+		private readonly LocalContext _context; // Använd din faktiska kontextklass
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-           
-        }
-        public List<Product> Products { get; set; }
-        public void OnGet()
-        {
-            Products = new List<Product>
-            {
-                new Product
-                {
-                    ProductName = "Cool tröja",
-                    ProductDescription = "Description",
-                    ProductPrice = 100,
-                    Quantity = 1,
-                    Rating = 3
-                }
-            };
+		public IndexModel(ILogger<IndexModel> logger, LocalContext context)
+		{
+			_logger = logger;
+			_context = context;
+		}
 
-        }
-    }
+		public List<Product> Products { get; set; }
+
+		public async Task OnGet()
+		{
+			Expression<Func<ProductEntity, bool>> expression = p => p.ProductPrice < 1000;
+
+			Products = await _context.Products
+				.Where(expression)
+				.Select(p => new Product
+				{
+					ProductName = p.ProductName,
+					ProductDescription = p.ProductDescription,
+					ProductPrice = p.ProductPrice,
+					Rating = p.Rating ?? 0,
+					Quantity = p.Quantity ?? 0
+				})
+				.ToListAsync();
+		}
+	}
 }
