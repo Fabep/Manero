688d96c35d0c7279aba302e47ecf84219073c94d
patpatpanda <nils-emil1337@hotmail.se>
2023-10-24 14:21:09 +0200

    final

--- ManeroWebApplication/Pages/Index.cshtml.cs
+++ ManeroWebApplication/Pages/Index.cshtml.cs
@@ -16,24 +16,12 @@ namespace ManeroWebApplication.Pages
             _productRepository = productRepository;
         }
 
-        public List<DataAccess.Models.Product> BestSellers { get; set; }
+        
         public List<DataAccess.Models.Product> FeaturedProducts { get; set; }
 
         public async Task OnGet()
         {
-           var productList = await _productRepository.GetAllAsync(x => x.ProductPrice < 900);
-
-            BestSellers = productList
-                .Select(p => new DataAccess.Models.Product
-                {
-                    ProductName = p.ProductName,
-                    ProductDescription = p.ProductDescription,
-                    ProductPrice = p.ProductPrice,
-                    Rating = p.Rating ?? 0,
-                    Quantity = p.Quantity ?? 0
-                })
-                .ToList();
-
+           
 
 
             var featuredProductList = await _productRepository.GetAllAsync(x => x.ProductPrice < 1000);
