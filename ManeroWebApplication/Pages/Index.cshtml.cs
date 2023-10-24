65ec81c9402bf5034839a5d8a62d3ae543918b22
patpatpanda <nils-emil1337@hotmail.se>
2023-10-24 14:54:30 +0200

    final

--- ManeroWebApplication/Pages/Index.cshtml.cs
+++ ManeroWebApplication/Pages/Index.cshtml.cs
@@ -7,35 +7,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ManeroWebApplication.Pages
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ProductRepository _productRepository;
+	public class IndexModel : PageModel
+	{
+		private readonly ProductRepository _productRepository;
 
-        public IndexModel( ProductRepository productRepository)
-        {
-            _productRepository = productRepository;
-        }
-
-        
-        public List<DataAccess.Models.Product> FeaturedProducts { get; set; }
-
-        public async Task OnGet()
-        {
-           
-
-
-            var featuredProductList = await _productRepository.GetAllAsync(x => x.ProductPrice < 1000);
-
-            FeaturedProducts = featuredProductList
-	            .Select(p => new DataAccess.Models.Product
-	            {
-		            ProductName = p.ProductName,
-		            ProductDescription = p.ProductDescription,
-		            ProductPrice = p.ProductPrice,
-		            Rating = p.Rating ?? 0,
-		            Quantity = p.Quantity ?? 0
-	            })
-	            .ToList();
+		public IndexModel(ProductRepository productRepository)
+		{
+			_productRepository = productRepository;
 		}
-    }
+
+		public List<DataAccess.Models.Product> FeaturedProducts { get; set; }
+
+		public async Task OnGet()
+		{
+			var featuredProductList = await _productRepository.GetAllAsync(x => x.ProductPrice < 1000);
+
+			FeaturedProducts = featuredProductList
+				.Select(p => new DataAccess.Models.Product
+				{
+					ProductName = p.ProductName,
+					ProductDescription = p.ProductDescription,
+					ProductPrice = p.ProductPrice,
+					Rating = p.Rating ?? 0,
+					Quantity = p.Quantity ?? 0
+				})
+				.ToList();
+		}
+	}
 }
