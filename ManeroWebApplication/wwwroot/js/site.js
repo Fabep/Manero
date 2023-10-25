a0870acc882f9863ba07e5498d90bcdd096624b8
Frederick <frederick.wennborg@gmail.com>
2023-10-25 16:04:42 +0200

    active class added when selecting Main Category button

--- ManeroWebApplication/wwwroot/js/site.js
+++ ManeroWebApplication/wwwroot/js/site.js
@@ -13,6 +13,27 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("mc-btns");
+var btns = header.getElementsByClassName("main-categories-nav-box-btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
     function openNav() {
         document.getElementById("navSidebar").style.width = "80%";
     document.getElementById("main").style.marginLeft = "250px";
