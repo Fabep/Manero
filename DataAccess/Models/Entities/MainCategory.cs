70e49340ecaaebc361ab30e801587651a3e1ce36
Frederick <frederick.wennborg@gmail.com>
2023-10-26 14:43:12 +0200

    MainCategoryEntity and ProductCategoryEntity added

---
+++ DataAccess/Models/Entities/MainCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DataAccess.Models.Entities.ProductCategoryEntity;
+
+namespace DataAccess.Models.Entities
+{
+    public class MainCategory
+    {
+        [Key]
+        public int MainCategoryID { get; set; }
+        public string MainCategoryName { get; set; }
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+    }
+}
---
+++ DataAccess/Models/Entities/ProductCategoryEntity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.Entities
+{
+    public class ProductCategoryEntity
+    {
+        public class ProductCategory
+        {
+            [Key]
+            public int ProductCategoryID { get; set; }
+            public string ProductCategoryName { get; set; }
+
+            public ICollection<MainCategory> MainCategories { get; set; }
+        }
+
+
+
+    }
+}
