b9dce488346f09e1d4f5dcd3eea61b2f8fef9b94
malin <mal3eine@hotmail.com>
2023-10-31 11:31:13 +0100

    Added linq query in productservice GetProductsFromSubCategory

--- DataAccess/Handlers/Services/ProductService.cs
+++ DataAccess/Handlers/Services/ProductService.cs
@@ -3,6 +3,7 @@ using DataAccess.Handlers.Repositories;
 using DataAccess.Handlers.Services.Abstractions;
 using DataAccess.Models;
 using DataAccess.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -27,6 +28,7 @@ namespace DataAccess.Handlers.Services
 		public List<Product> ProductsFromSubCategory { get; set; }
 
 
+
 		public async Task GetAllBestSellersAsProducts()
 		{
 			var productList = await _productRepository.GetAllAsync(x => x.ProductPrice < 900);
@@ -35,10 +37,16 @@ namespace DataAccess.Handlers.Services
 			 .Select(p => DataConverter.ConvertProductEntityToProduct(p))
 			 .ToList();
 		}
+
 		public async Task<List<Product>> GetProductsFromSubCategory(string subProductCategory)
 		{
 			//var productList = _subCategoryRepository....
-			//var productList = _productRepository.GetAllAsync(x => x. ); // vill hämta de produkter som tillhör vald subkategori
+
+			// vill hämta de produkter som tillhör vald subkategori
+			var productList = await _productRepository.GetAllAsync(x => x.GetType() == typeof(ProductEntity));
+			
+
+			ProductsFromSubCategory = productList.AsQueryable().Include(a => a.SubCategoryEntity()).ToList();
 
 			//ProductsFromSubCategory = productList
 			//	.Select(productList => DataConverter.ConvertProductEntityToProduct(p))
