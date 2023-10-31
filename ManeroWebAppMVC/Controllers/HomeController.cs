c1a9d0b57bebdc5ada1af140f7c699e377eb1ae6
Frederick <frederick.wennborg@gmail.com>
2023-10-31 18:03:05 +0100

    added compiled css files

--- ManeroWebAppMVC/Controllers/HomeController.cs
+++ ManeroWebAppMVC/Controllers/HomeController.cs
@@ -5,11 +5,9 @@ namespace ManeroWebAppMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
