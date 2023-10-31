7af37166f4062b902476d30f835d4783f3b7b57d
Frederick <frederick.wennborg@gmail.com>
2023-10-31 20:51:57 +0100

    added Article IAction result and fixed routing from ProductTile

--- ManeroWebAppMVC/Controllers/ProductsController.cs
+++ ManeroWebAppMVC/Controllers/ProductsController.cs
@@ -1,4 +1,6 @@
-﻿using DataAccess.Handlers.Services.Abstractions;
+﻿using DataAccess.ExtensionMethods;
+using DataAccess.Handlers.Repositories;
+using DataAccess.Handlers.Services.Abstractions;
 using DataAccess.Models.ViewModels;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Mvc;
@@ -8,14 +10,17 @@ namespace ManeroWebAppMVC.Controllers
 	public class ProductsController : Controller
 	{
 		private readonly IProductService _productService;
-
-		public ProductsController(IProductService productService)
-		{
-			_productService = productService;
-		}
+        private readonly ProductRepository _productRepository;
 
 
-		public async Task<IActionResult> Index(string subProductCategory)
+        public ProductsController(IProductService productService, ProductRepository productRepository)
+        {
+            _productService = productService;
+            _productRepository = productRepository;
+        }
+
+
+        public async Task<IActionResult> Index(string subProductCategory)
 		{
 
 			var viewModel = new ProductsViewModel
@@ -26,5 +31,20 @@ namespace ManeroWebAppMVC.Controllers
 
 			return View(viewModel);
 		}
-	}
+
+
+        public async Task<IActionResult> Article(Guid id)
+        {
+
+            var viewModel = new ArticleViewModel
+            {
+                Product = DataConverter.ConvertProductEntityToProduct(
+                await _productRepository.GetAsync(x => x.ProductId == id)),
+        };
+
+            return View(viewModel);
+        }
+
+
+    }
 }
