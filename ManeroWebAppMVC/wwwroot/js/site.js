3d2ef86c8a6dd932c63abc4e7ac774f8109b0738
Frederick <frederick.wennborg@gmail.com>
2023-11-07 15:58:11 +0100

    products adds to list in cart

--- ManeroWebAppMVC/wwwroot/js/site.js
+++ ManeroWebAppMVC/wwwroot/js/site.js
@@ -14,16 +14,6 @@ function remove() {
         currentAmountElement.innerHTML = currentAmount;
     }
 }
-// Add active class to the current selected Main Category button (highlight it)
-var header = document.getElementById("mc-btns");
-var btns = header.getElementsByClassName("main-categories-nav-box-btn");
-for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function () {
-        var current = document.getElementsByClassName("active");
-        current[0].className = current[0].className.replace(" active", "");
-        this.className += " active";
-    });
-}
 
 
 
@@ -51,7 +41,7 @@ function closeCart() {
 
 
 
-
+// ADD TO CART FUNCTIONS
 const btnCart = document.querySelector('#cart-icon');
 const cart = document.querySelector('.cart');
 
@@ -62,30 +52,6 @@ const cart = document.querySelector('.cart');
 //    cartCount++;
 //    document.getElementById('cart-count').innerText = cartCount;
 //}
-
-
-function loadContent() {
-    //RemoveItems  From Cart
-    let btnRemove = document.querySelectorAll('.cart-remove');
-    btnRemove.forEach((btn) => {
-        btn.addEventListener('click', removeItem);
-    });
-
-    //Product Item Change Event
-    let qtyElements = document.querySelectorAll('.cart-quantity');
-    qtyElements.forEach((input) => {
-        input.addEventListener('change', changeQty);
-    });
-
-    //Product Cart
-
-    let cartBtns = document.querySelectorAll('.add-cart');
-    cartBtns.forEach((btn) => {
-        btn.addEventListener('click', addCart);
-    });
-
-    updateTotal();
-}
 
 
 
@@ -105,6 +71,82 @@ function loadContent() {
 
 // Add a click event listener to the "buy" icon
 
+
+let productList = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    const buyIcons = document.querySelectorAll('.fa-solid.fa-bag-shopping.buy');
+
+    buyIcons.forEach(function (buyIcon) {
+        buyIcon.addEventListener('click', function () {
+            let productTile = this.closest('.product-tile');
+            let title = productTile.querySelector('.product-name').innerText;
+            let price = productTile.querySelector('.product-price').innerText;
+            let imgSrc = productTile.querySelector('.product-image img').src;
+
+            // Call the function to add the product to the list
+            addProductToList(title, price, imgSrc);
+        });
+    });
+
+    // Define the function to add the product to the list
+    function addProductToList(title, price, imgSrc) {
+        let newProduct = { title, price, imgSrc };
+
+        productList.push(newProduct);
+        console.log('Product added to the list:', newProduct);
+
+        let newProductElement = createCartProduct(title, price, imgSrc);
+
+        let element = document.createElement('div');
+        element.innerHTML = newProductElement;
+        let cartBasket = document.querySelector('.cart-content');
+        cartBasket.append(element);
+
+        loadContent();
+    }
+});
+
+
+function createCartProduct(title, price, imgSrc) {
+
+    return `
+         <div class="cart-box">
+                <img src="${imgSrc}" class="cart-img">
+             <div class="detail-box">
+                <div class="cart-food-title">${title}</div>
+                <div class="price-box">
+                      <div class="cart-price">${price}</div>
+                      <div class="cart-amt">${price}</div>
+                </div>
+             <input type="number" value="1" class="cart-quantity">
+             </div>
+         </div>
+           `;
+}
+
+
+function loadContent() {
+    //RemoveItems  From Cart
+    let btnRemove = document.querySelectorAll('.cart-remove');
+    btnRemove.forEach((btn) => {
+        btn.addEventListener('click', removeItem);
+    });
+
+    //Product Item Change Event
+    let qtyElements = document.querySelectorAll('.cart-quantity');
+    qtyElements.forEach((input) => {
+        input.addEventListener('change', changeQty);
+    });
+
+    //Product Cart
+    let cartBtns = document.querySelectorAll('.add-cart');
+    cartBtns.forEach((btn) => {
+        btn.addEventListener('click', addCart);
+    });
+
+    updateTotal();
+}
 
 
 
