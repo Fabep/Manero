52f5cbf93a3ef4d0b42516a517640dad1df9c0c3
Frederick <frederick.wennborg@gmail.com>
2023-11-09 12:28:50 +0100

    removed comment

--- ManeroWebAppMVC/wwwroot/js/site.js
+++ ManeroWebAppMVC/wwwroot/js/site.js
@@ -36,18 +36,7 @@ function addSelectedForButton(target, containerId, className) {
     }
     document.getElementById(target).classList.add("selected");
 }
-// Add active class to the current selected Main Category button (highlight it)
-//var header = document.getElementById("mc-btns");
-//var btns = header.getElementsByClassName("main-categories-nav-box-btn");
-//for (var i = 0; i < btns.length; i++) {
-//    btns[i].addEventListener("click", function () {
-//        var current = document.getElementsByClassName("active");
-//        current[0].className = current[0].className.replace(" active", "");
-//        this.className += " active";
-//    });
-//}
 
-//Open and close sidebar nav
 function openNav() {
     document.getElementById("navSidebar").style.width = "100%";
     document.getElementById("navSidebar").style.maxWidth = "400px";
