12bf915b39e3ca66e69804da712c33250883349f
malin <mal3eine@hotmail.com>
2023-11-09 15:01:58 +0100

    minor change

--- DataAccess/Models/ViewModels/WishListViewModel.cs
+++ DataAccess/Models/ViewModels/WishListViewModel.cs
@@ -9,6 +9,19 @@ namespace DataAccess.Models.ViewModels
     public class WishListViewModel
     {
         public string PageTitle { get; set; } = "Wish list";
-        public List<Product> Products { get; set; } = new List<Product>();
+        public List<Product> Products { get; set; } = new List<Product>
+        {
+            new Product
+            {
+                ProductId = new Guid(),
+                ProductName = "Yellow Skirt",
+                ProductPrice = 100,
+                ProductDescription = "lahfjlaf",
+                DiscountedPrice = 75,
+                Rating = 4,
+                Quantity = 1,
+
+            }
+        };
     }
 }
