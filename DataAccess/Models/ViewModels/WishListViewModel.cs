c45e7f23040efd74f0305935a8407453875b0d84
malin <mal3eine@hotmail.com>
2023-11-09 11:21:21 +0100

    Added wishList view, controller and viewmodel

---
+++ DataAccess/Models/ViewModels/WishListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.ViewModels
+{
+    public class WishListViewModel
+    {
+        public string PageTitle { get; set; } = "Wish list";
+        public List<Product>? Products { get; set; }
+    }
+}
---
+++ ManeroWebAppMVC/Controllers/WishListController.cs
@@ -0,0 +1,25 @@
+﻿using DataAccess.Handlers.Services.Abstractions;
+using DataAccess.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManeroWebAppMVC.Controllers
+{
+	public class WishListController : Controller
+	{
+
+		private readonly IProductService _productService;
+		public WishListController(IProductService productService)
+		{
+			_productService = productService;
+		}
+		public IActionResult Index()
+		{
+			var viewModel = new WishListViewModel();
+
+			//viewModel.Products = _productService.AddProductToWishList();
+			//viewModel.Products = _productService.DeleteProductFromWishList();
+
+			return View(viewModel);
+		}
+	}
+}
