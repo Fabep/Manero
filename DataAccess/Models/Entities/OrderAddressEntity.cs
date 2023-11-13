b727d49318cd6f5a3c3da27f65b481f5a6c228ee
patpatpanda <nils-emil1337@hotmail.se>
2023-11-13 12:04:48 +0100

    added classes

---
+++ DataAccess/Models/Entities/OrderAddressEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.Entities
+{
+	public class OrderAddressEntity
+	{
+		[Key]
+		public int Id { get; set; } 
+		public int OrderId { get; set; } // [fk]
+		public string? AddressType { get; set; }
+		public string? StreetAddress { get; set; }
+		public string? Streetnumber { get; set; }
+		public string? PostalCode { get; set; }
+		public string? City { get; set; }
+		public string? Region { get; set; }
+		public string? Country { get; set; }
+	}
+}
---
+++ DataAccess/Models/Entities/PaymentMethodEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.Entities
+{
+	public class PaymentMethodEntity
+	{
+		[Key]
+		public int PaymentId { get; set; } 
+		public int CardNumber { get; set; }
+	}
+}
