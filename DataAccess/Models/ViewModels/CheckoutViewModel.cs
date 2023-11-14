f5ae092a6a135468afe6d8eaa4e3440d3a5e0adf
malin <mal3eine@hotmail.com>
2023-11-14 11:52:54 +0100

    Added orderviewmodels and view

---
+++ DataAccess/Models/ViewModels/CheckoutViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models.ViewModels
+{
+    public class CheckoutViewModel
+    {
+        public decimal TotalOrderSum { get; set; }
+        public List<CheckOutArticleViewModel>? Products { get; set; } = new List<CheckOutArticleViewModel>(); 
+        public decimal OrderDiscount { get; set; }
+        public string? DeliveryFee { get; set; }
+        public string? ShippingDetails { get; set; }
+        public string? PaymentMethod { get; set; }
+        public string? UserComment { get; set; }
+    }
+}
--- ManeroWebAppMVC/Controllers/OrderController.cs
+++ ManeroWebAppMVC/Controllers/OrderController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ManeroWebAppMVC.Controllers
 {
     public class OrderController : Controller
     {
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Checkout()
+        {
+            var viewModel = new CheckoutViewModel();
+            viewModel.TotalOrderSum = 0;
+            //viewModel.Products = _service.GetOrderProducts();
+            viewModel.OrderDiscount = 0;
+            viewModel.DeliveryFee = "Free";
+            viewModel.ShippingDetails = "1006 Telefonvägen";
+            viewModel.PaymentMethod = "Kort nr 348620";
+
+            return View(viewModel);
         }
     }
 }
