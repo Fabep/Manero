f443dbb0cfa1130bf15af19c86d2112b1104faf5
malin <mal3eine@hotmail.com>
2023-11-14 12:56:09 +0100

    Added orderrepo

---
+++ DataAccess/Handlers/Repositories/OrderRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataAccess.Contexts;
+using DataAccess.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Handlers.Repositories
+{
+    public class OrderRepository : Repository<OrdersEntity, LocalContext>
+    {
+        public OrderRepository(LocalContext context) : base(context)
+        {
+        }
+    {
+    }
+}
---
+++ DataAccess/Handlers/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Handlers.Repositories;
+using DataAccess.Handlers.Services.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Handlers.Services
+{
+    public class OrderService : IOrderService
+    {
+
+        private readonly OrderRepository _orderRepository;
+
+        public OrderService(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+
+
+    }
+}
