a4a22f5d342ab0ee4fcf54661396e600868c2b84
Frederick <frederick.wennborg@gmail.com>
2023-11-16 17:18:47 +0100

    added change quantity function On Order page

--- ManeroWebAppMVC/Controllers/OrderController.cs
+++ ManeroWebAppMVC/Controllers/OrderController.cs
@@ -28,6 +28,11 @@ namespace ManeroWebAppMVC.Controllers
                 var productCookie = _cookieService.GetCookie(Request, "ProductsCookie");
                 var cartList = JsonConvert.DeserializeObject<List<ProductCartObject>>(productCookie!);
 
+                if (cartList.Count == 0 || cartList is null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
 
                 var order = new OrderSchema();
                 order.Items = cartList;
@@ -55,7 +60,35 @@ namespace ManeroWebAppMVC.Controllers
             }
             return View();
         }
-       
+
+        [HttpPost]
+        public IActionResult ChangeQuantity(Guid productId, string operation)
+        {
+            var cartList = JsonConvert.DeserializeObject<List<ProductCartObject>>(_cookieService.GetCookie(Request, "ProductsCookie")!);
+
+            if (cartList!.Select(x => x.ProductId).Contains(productId))
+            {
+
+                if (operation == "increase")
+                {
+                    cartList!.FirstOrDefault(x => x.ProductId == productId)!.Quantity += 1;
+                }
+                else if (operation == "decrease" && cartList!.FirstOrDefault(x => x.ProductId == productId)!.Quantity > 1)
+                {
+                    cartList!.FirstOrDefault(x => x.ProductId == productId)!.Quantity += -1;
+                }
+
+                _cookieService.AddCookie(Response, "ProductsCookie", cartList);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+
+
+
+
+
         [HttpGet]
         public async Task<IActionResult> ShippingDetails(int? cid)
         {
