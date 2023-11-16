258cc0a28b717aab261c93e45c1da160c6a6b1bb
malin <mal3eine@hotmail.com>
2023-11-16 16:28:43 +0100

    update

--- ManeroWebAppMVC/Controllers/OrderController.cs
+++ ManeroWebAppMVC/Controllers/OrderController.cs
@@ -88,60 +88,60 @@ namespace ManeroWebAppMVC.Controllers
 
 
 
-        //public async Task<IActionResult> SubmitCheckout(OrderSchema order)
-        //{
-        //    if (order is not null && ModelState.IsValid)
-        //    {
-        //        //verifiera order i service
-        //        if (order.PaymentMethod != null && order.TotalAmount > 0
-        //            && order.BillingAddressSchema != null && order.DeliveryAddressSchema != null
-        //            && order.Items?.Count > 0)
-        //        {
+        public async Task<IActionResult> SubmitCheckout(OrderSchema order)
+        {
+            if (order is not null && ModelState.IsValid)
+            {
+                //verifiera order i service
+                if (order.PaymentMethod != null && order.TotalAmount > 0
+                    && order.BillingAddressSchema != null && order.DeliveryAddressSchema != null
+                    && order.Items?.Count > 0)
+                {
 
-        //            // orderSchema till orderEntity gör om i en orderservice 
-        //            var orderEntity = DataConverter.ConvertOrderSchemaToOrderEntity(order);
-        //            foreach (var item in order.Items)
-        //            {
-        //                // orderitems är productCartObjet och ska sparas i databas som orderitem
-        //                var orderItemEntity = new OrderItemsEntity();
-        //                orderItemEntity.ProductId = item.ProductId;
+                    // orderSchema till orderEntity gör om i en orderservice 
+                    var orderEntity = DataConverter.ConvertOrderSchemaToOrderEntity(order);
+                    foreach (var item in order.Items)
+                    {
+                        // orderitems är productCartObjet och ska sparas i databas som orderitem
+                        var orderItemEntity = new OrderItemsEntity();
+                        orderItemEntity.ProductId = item.ProductId;
 
 
-        //                /*
-        //                orderitem
-        //                    public int OrderItermsId { get; set; } - skapas automatiskt?
-        //                    public int OrderId { get; set; } - från ordern
-		      //              public OrdersEntity? Order { get; set; }
-		      //              public int ProductId { get; set; } -  productid från productcartobject
-		      //              public string? ProductName { get; set; } - från productcartobject
-		      //              public decimal TotalAmount { get; set; } - quantity * price från productcartobject
-        //                    public decimal DiscountPrice { get; set; } - quantity * discount price från productcartobject
-		      //              public int Quantity { get; set; } - quantity från productcartobject
+                        /*
+                        orderitem
+                            public int OrderItermsId { get; set; } - skapas automatiskt?
+                            public int OrderId { get; set; } - från ordern
+		                    public OrdersEntity? Order { get; set; }
+		                    public int ProductId { get; set; } -  productid från productcartobject
+		                    public string? ProductName { get; set; } - från productcartobject
+		                    public decimal TotalAmount { get; set; } - quantity * price från productcartobject
+                            public decimal DiscountPrice { get; set; } - quantity * discount price från productcartobject
+		                    public int Quantity { get; set; } - quantity från productcartobject
 
-        //                 productcartobjet
-        //                    public Guid ProductId { get; set; }
-        //                    public string ProductName { get; set; } = null!;
-        //                    public double Price { get; set; }
-        //                    public double DiscountedPrice { get; set; }
-        //                    public string Size { get; set; } = null!;
-        //                    public string Color { get; set; } = null!;
-        //                    public int Quantity { get; set; }
-        //                    public string ImageUrl { get; set; } = null!;
+                         productcartobjet
+                            public Guid ProductId { get; set; }
+                            public string ProductName { get; set; } = null!;
+                            public double Price { get; set; }
+                            public double DiscountedPrice { get; set; }
+                            public string Size { get; set; } = null!;
+                            public string Color { get; set; } = null!;
+                            public int Quantity { get; set; }
+                            public string ImageUrl { get; set; } = null!;
 
                  
-        //                */
+                        */
 
 
 
-        //            }
+                    }
 
-        //            // spara  i databas
+                    // spara  i databas
 
-        //            return RedirectToAction("");// gå till sida för att se om ordern lyckats
-        //        }
-        //    }
-        //    return View();
-        //}
+                    return RedirectToAction("");// gå till sida för att se om ordern lyckats
+                }
+            }
+            return View();
+        }
 
         [HttpGet]
         public async Task<IActionResult> Checkout(OrderSchema schema)
