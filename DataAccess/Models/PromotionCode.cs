90f8ea64e8b1558a94fb4b724385262605776248
Frederick <frederick.wennborg@gmail.com>
2023-11-23 14:28:59 +0100

    added promotioncode converter

--- DataAccess/Models/PromotionCode.cs
+++ DataAccess/Models/PromotionCode.cs
@@ -5,22 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Models.Entities
+namespace DataAccess.Models
 {
-    public class PromotionCodeEntity
+    public class PromotionCode
     {
-        [Key]
-        public int Id { get; set; }
+
         public string Name { get; set; } = null!;
         public decimal DiscountRate { get; set; }
 
         public string? Description { get; set; }
 
-        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
 
-        [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
-
     }
 }
