cf05e92e5510966c381b23768041a378973f2358
Frederick <frederick.wennborg@gmail.com>
2023-11-27 13:56:42 +0100

    added category tests

--- DataAccess.Tests/Handlers/Services/CategoryServiceTests.cs
+++ DataAccess.Tests/Handlers/Services/CategoryServiceTests.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DataAccess.Contexts;
+using DataAccess.Handlers.Repositories;
+using DataAccess.Handlers.Services;
+using DataAccess.Handlers.Services.Abstractions;
+using DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +13,93 @@ namespace DataAccess.Tests.Handlers.Services
 {
     public class CategoryServiceTests
     {
-        //[Fact]
-        //public void Test1()
-        //{
-        //    //Arrange
+        private ICategoryService _sut;
+        private SubCategoryRepository _subCategoryRepository;
+        private LocalContext _localContext;
 
-        //    //Act
+        public CategoryServiceTests()
+        {
+            _localContext = new LocalContext();
+            _subCategoryRepository = new SubCategoryRepository(_localContext);
+            _sut = new CategoryService(_subCategoryRepository);
 
-        //    //Assert
-        //}
+        }
+
+
+        [Fact]
+        public async Task GetAllPrimaryCategories_ReturnsListOfPrimaryCategories()
+        {
+            //Arrange
+
+            //Act
+            var result = await _sut.GetAllPrimaryCategories();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<PrimaryCategory>>(result);
+            Assert.Contains(result, p => p.PrimaryCategoryName.Contains("Men"));
+            Assert.Contains(result, p => p.PrimaryCategoryName.Contains("Women"));
+            Assert.Contains(result, p => p.PrimaryCategoryName.Contains("Unisex"));
+
+        }
+
+
+        [Fact]
+        public async Task GetSubCategoriesByPrimaryCategoryIdMen_ReturnsListOfSubCategories_AssociatedWithPrimaryCategoryId()
+        {
+            //Arrange
+            var primaryCategoryId = 1;
+
+
+            //Act
+            var result = await _sut.GetSubCategoriesByPrimaryCategoryId(primaryCategoryId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<SubCategory>>(result);
+            Assert.Contains(result, p => p.SubCategoryName.Contains("Men"));
+            Assert.Contains(result, p => p.SubCategoryName.Contains("Suits"));
+
+
+        }
+
+
+        [Fact]
+        public async Task GetSubCategoriesByPrimaryCategoryIdWomen_ReturnsListOfSubCategories_AssociatedWithPrimaryCategoryId()
+        {
+            //Arrange
+            var primaryCategoryId = 2;
+
+
+            //Act
+            var result = await _sut.GetSubCategoriesByPrimaryCategoryId(primaryCategoryId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<SubCategory>>(result);
+            Assert.Contains(result, p => p.SubCategoryName.Contains("Women"));
+            Assert.Contains(result, p => p.SubCategoryName.Contains("Dresses"));
+
+
+        }
+
+
+
+        [Fact]
+        public async Task GetSubCategoriesByPrimaryCategoryIdUnisex_ReturnsListOfSubCategories_AssociatedWithPrimaryCategoryId()
+        {
+            //Arrange
+            var primaryCategoryId = 3;
+
+
+            //Act
+            var result = await _sut.GetSubCategoriesByPrimaryCategoryId(primaryCategoryId);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<SubCategory>>(result);
+            Assert.Contains(result, p => p.SubCategoryName.Contains("Unisex"));
+        }
+
     }
 }
